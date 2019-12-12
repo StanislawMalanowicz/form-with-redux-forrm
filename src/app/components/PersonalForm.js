2e77958f849b663d2961922aca7f7bf56e76b9f3
Your Name <you@example.com>
2019-12-12 18:40:30 +0100

    validate begin

--- src/app/components/PersonalForm.js
+++ src/app/components/PersonalForm.js
@@ -1,36 +1,79 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 
+ const validate = values => {
+   const errors = {};
+
+//    if (!values.password) {
+//      errors.password = "Required";
+//    } else if (values.password.length < 6) {
+//      errors.password = "Must be 6 characters or more";
+//    }
+
+   if(!values.secondPassword){
+       errors.secondPassword = "Required";
+   } else if(values.password !== values.secondPassword){
+       errors.secondPassword = "Wrong Password";
+   }
+
+   return errors;
+ };
+
+const renderField = ({
+  input,
+  label,
+  type,
+  meta: { touched, error, warning }
+}) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type} />
+      {touched &&
+        ((error && <span>{error}</span>) ||
+          (warning && <span>{warning}</span>))}
+    </div>
+  </div>
+);
+
+
 let ContactForm = props => {
-  const { handleSubmit } = props;
+  const { handleSubmit, dirty, submitting } = props;
 
   const submit = values => {
-      // print the form values to the console
-      console.log(values);
+
+    //   console.log(values);
+      return new Promise((resolve, reject) => {
+          setTimeout(() => {
+              console.log(values)
+              resolve()
+          }, 2000)
+      })
     };
+
+   
 
   return (
     <form onSubmit={handleSubmit(submit)}>
-      <div>
-        <label htmlFor="firstName">First Name</label>
-        <Field name="firstName" component="input" type="text" />
-      </div>
-      <div>
-        <label htmlFor="lastName">Last Name</label>
-        <Field name="lastName" component="input" type="text" />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <Field name="email" component="input" type="email" />
-      </div>
-      <button type="submit">Submit</button>
+        {/* <Field name="email" type="email" component={renderField} label="Email" /> */}
+      <Field
+        name="password"
+        type="password"
+        component={renderField}
+        label="Password"
+      />
+      <Field name="secondPassword" type="password" component={renderField} label="Confirm Password" />
+      <button type="submit" disabled={!dirty || submitting}>
+        Submit
+      </button>
     </form>
   );
 };
 
 ContactForm = reduxForm({
   // a unique name for the form
-  form: "contact"
+  form: "contact",
+  validate
 })(ContactForm);
 
 export default ContactForm;
